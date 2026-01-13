310f26397fa749893bc3e58b67c00c35ad884b2b
Nadir <raremammadov@gmail.com>
2026-01-13 23:54:01 +0400

    Remove unused import in firebase config

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from "firebase/storage";
